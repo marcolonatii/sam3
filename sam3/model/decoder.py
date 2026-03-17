b3d7f694eb2c3f20ef80cf541933302cf4ba725e
Hiroki Kawai <h.kawai888@gmail.com>
2026-03-17 17:33:12 +0900

    Fix hardcoded autocast device_type="cuda" in forward_ffn for CPU/MPS support

--- sam3/model/decoder.py
+++ sam3/model/decoder.py
@@ -68,7 +68,8 @@ class TransformerDecoderLayer(nn.Module):
         return tensor if pos is None else tensor + pos
 
     def forward_ffn(self, tgt):
-        with torch.amp.autocast(device_type="cuda", enabled=False):
+        device_type = tgt.device.type if tgt.device.type != "mps" else "cpu"
+        with torch.amp.autocast(device_type=device_type, enabled=False):
             tgt2 = self.linear2(self.dropout3(self.activation(self.linear1(tgt))))
         tgt = tgt + self.dropout4(tgt2)
         tgt = self.norm3(tgt)
